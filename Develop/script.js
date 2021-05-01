aca0e0dedcd01edc62d211ba7c258045c4674e27
Jeremy's commits <jeremyhurst16@gmail.com>
2021-05-01 01:30:31 -0400

    added functions to get parameters/called the function for password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,42 +1,44 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate')
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = generatePassword()
+  var passwordText = document.querySelector('#password')
 
-  passwordText.value = password;
-
+  passwordText.value = password
 }
 
-
-
-
+function generatePassword(
+  length,
+  inludeLowercase,
+  includeUppercase,
+  includeNumeric,
+  includeSpecial,
+) {
+  // to-do: implement password generator
+}
 
 //choose the length
-var writePassword = window.prompt(
-  'How many character would you like your password to be? (8-128 characters')
 
-  
+function promptLength() {
+  var writePassword = window.prompt(
+    'How many character would you like your password to be? (8-128 characters',
+  )
+
   if (writePassword <= 7 || writePassword > 128) {
     window.alert('Please choose a number between 8-128')
   }
- 
+  return writePassword;
+}
+
+
+generatePassword(
+  promptLength(),
+)
 
 
 //choose uppercase lowercase numeric etc
 
-if (writePassword === uppercase || writePassword === UPPERCASE) {
-
-}
-
-
-
-
-
-
-
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword)
